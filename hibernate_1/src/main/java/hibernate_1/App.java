89114c7281b9d0e6ddd10e4ba47e4808e2f9dcab
Rajkumar <rk235216.rk@gmail.com>
2019-08-10 10:30:02 +0530

    created app class with main and added details for the first student.

---
+++ hibernate_1/src/main/java/hibernate_1/App.java
@@ -0,0 +1,15 @@
+package hibernate_1;
+
+public class App {
+	
+	public static void main() 
+	{
+		Student cs = new Student();
+		cs.setName("Raj");
+		cs.setId(1);
+		cs.setBranch("cse");
+		cs.setDepartment("cs");
+		
+	}
+
+}
